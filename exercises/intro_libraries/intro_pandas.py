2e0085a42c3fea80a30d7215b765124eac179a7b
christiantrejoaguillon2-commits <christiantrejoaguillon2@gmail.com>
2025-09-25 22:21:15 -0600

    intento 40

--- exercises/intro_libraries/intro_pandas.py
+++ exercises/intro_libraries/intro_pandas.py
@@ -50,7 +50,7 @@ above_nine = csv_df[csv_df["promedio"] > 9]
 plog(f"Estudiantes con promedio > 9: {above_nine}", level=ERROR if above_nine is None else DEBUG, eol=True)
 
 # Ejercicio 06: Agrupamiento y estadísticas
-career_group = csv_data.groupby('carrera')
+career_group = csv_data.__int__('carrera')
 general_mean = career_group['promedio'].mean()
 plog(f"Promedio por carrera: {general_mean}", level=ERROR if general_mean is None else DEBUG, eol=True)
 
