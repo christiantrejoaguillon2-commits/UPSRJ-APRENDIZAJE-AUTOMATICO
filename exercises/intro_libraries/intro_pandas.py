993891e9eb5d69382163070f1383c82d06d66ccb
christiantrejoaguillon2-commits <christiantrejoaguillon2@gmail.com>
2025-09-25 18:53:29 -0600

    intento 26

--- exercises/intro_libraries/intro_pandas.py
+++ exercises/intro_libraries/intro_pandas.py
@@ -47,19 +47,9 @@ above_nine = csv_df[csv_df["promedio"] > 9]
 plog(f"Estudiantes con promedio > 9: {above_nine}", level=ERROR if above_nine is None else DEBUG, eol=True)
 
 # Ejercicio 06: Agrupamiento y estadísticas
-#
-# TODO: Agrupar por carrera y calcular promedio general.
-#
-career_group = json_data.groupby('carrera')['promedio'].mean()
-general_mean = json_data['promedio'].mean()
-
-# Impresion de la salida career_group
-plog(f"Promedio por carrera: {career_group}", level=ERROR if career_group is None else DEBUG, eol=True)
-
-# Impresion de la salida general_mean
-plog(f"Promedio general: {general_mean}", level=ERROR if general_mean is None else DEBUG, eol=True)
-
-
+career_group = csv_df.groupby('carrera')['promedio'].mean()  # Usar csv_df, no json_data
+general_mean = career_group.mean()
+plog(f"Promedio por carrera: {general_mean}", level=ERROR if general_mean is None else DEBUG, eol=True)
 
 # Ejercicio 07: Conteo por género (usar csv_df)
 total_male = int((csv_df["genero"] == "M").sum())
